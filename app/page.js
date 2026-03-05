9e3aaf268d90ba742eaa7cc1b3db05482b28767c
schausse-hash <schausse@gmail.com>
2026-03-05 01:16:06 -0500

    fix routing: remove root [slug], redirect / to /fr

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 
-export default function RootPage() {
-  // Default language
-  redirect('/fr')
+export default function Home() {
+  redirect('/fr');
 }
